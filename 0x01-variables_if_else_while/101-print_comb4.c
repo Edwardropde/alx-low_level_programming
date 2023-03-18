878534f8f22b9f8eda122391df7547631b6cbf54
rophas <rophasedward@gmail.com>
2023-03-18 11:09:26 +0000

    changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,23 +13,28 @@ int main(void)
 	{
 		for (j = 49; j < 58; j++)
 		{
-			for (k = 50; k < 58; k++)
+			for (k = 50; k < 57; k++)
 			{
-				if (k > j && j > K)
+				if (i != j && i != k && j != k)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
-				}
-				if (i != 55 || j != 56)
-				{
-					putchar(',');
-					putchar(' ');
+					if (k > j && j > i)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+
+						if (i != 55 || j != 56 || k != 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
+
+						
