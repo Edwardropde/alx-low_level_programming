0ade8f78a0d7e94296f0b92acc3d73d6a8c01497
rophas <rophasedward@gmail.com>
2023-03-18 01:58:38 +0000

    printbase16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ *
+ * Return: 0 if successeful
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + 'a');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+	
+
