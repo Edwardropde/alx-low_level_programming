cf88eb51fc6dd93d0accbc2b7aa494b0010233df
rophas <rophasedward@gmail.com>
2023-03-18 00:42:00 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ *
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
