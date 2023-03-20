42a34dfa47d70d8ffea8d55e2a8b1863445be485
rophas <rophasedward@gmail.com>
2023-03-20 06:45:42 +0000

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,13 @@ int main(void)
 	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
-	}
-	if (a != 9)
-	{
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('$');
+	if (a != 9)
+	{
+		putchar('.');
+	}
+	putchar('\n');
 	return (0);
 }
