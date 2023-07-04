76f420f8b004db2d7f7154f58bbeedf398c08d06
Edwardropde <edwardropha@gmail.com>
2023-07-04 14:47:49 +0000

    more singled linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * find_listint_loop - Find loop in the listint_t linked list
+ * @head: Points to the head or address of listint_t list
+ * Return: NULL if no loop and otherwise if address of first node of list found
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+	listint_t *tortoise = head, *hare = head;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+	return (NULL);
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *thenode;
+
+	if (head == NULL)
+		return;
+	for (; *head != NULL; *head = thenode)
+	{
+		thenode = (*head)->next;
+		free(*head);
+	}
+	*head = NULL;
+}
